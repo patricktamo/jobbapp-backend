2cfd52fbbb5b829ea519cdd9f63be51fda9fd137
Patrick Tamo <tamo.patson@gmail.com>
2024-11-14 19:12:46 -0500

    Set the Db context

---
+++ Database/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace Backend.Database;
+
+public class ApplicationDbContext: DbContext
+{
+	public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+	{
+	}
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        base.OnConfiguring(optionsBuilder);
+        if (!optionsBuilder.IsConfigured)
+        {
+            optionsBuilder.UseMySql("Server=localhost;Database=jobapp_db;User=root;Password=AmehisIkem;", new MySqlServerVersion(new Version(8, 4, 3)));
+        }
+    }
+}
